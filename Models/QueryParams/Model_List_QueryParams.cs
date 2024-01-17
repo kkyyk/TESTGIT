9acc8f44367aeaaaf0b3cd7d6357163725bbdb2b
kkyyk <k1991666@gmail.com>
2024-01-17 22:50:33 +0800

    查詢機制

---
+++ Models/QueryParams/Model_List_QueryParams.cs
@@ -0,0 +1,12 @@
+﻿namespace TESTMVCCORE.Models.QueryParams
+{
+	public class Model_List_QueryParams
+	{
+		public int? Age { get; set; } //年齡
+
+		public string? NumType { get; set; } //查詢方式(0:小於 1:大於)
+		public string? Type { get; set; } //種類
+
+		public string? NameKeyword { get; set; } //名字關鍵字
+	}
+}
